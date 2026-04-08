f0b51cb901dcf334818059fb8c77cca6536a7f24
German Ruiz <germanruzca@gmail.com>
2026-04-08 14:38:25 -0600

    client: add styles theme

---
+++ client/styles/theme.ts
@@ -0,0 +1,27 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#F5F3FF",
+    secondary: "#EDE9FE",
+    text: "#1C1917",
+    primaryLight: "#0070f3",
+    textMuted: "#666",
+    navBarBackground: "#8B5CF6",
+    white: "#fff",
+    border: "#ddd",
+  },
+  borderRadius: {
+    sm: "4px",
+    md: "8px",
+    lg: "16px",
+    full: "9999px",
+  },
+  shadows: {
+    sm: "0 1px 2px rgba(0, 0, 0, 0.05)",
+    md: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    lg: "0 10px 15px rgba(0, 0, 0, 0.2)",
+  },
+};
+
+export default theme;
