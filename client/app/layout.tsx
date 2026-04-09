9ef162d4b432565432ff98f3df456a789706fa9c
German Ruiz <germanruzca@gmail.com>
2026-04-08 18:10:22 -0600

    client: minimalize app page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Providers from '@/components/layout/Providers';
 import StyledComponentsRegistry from '@/lib/styled-components-registry';
-import Navbar from '@/components/layout/Navbar/Navbar';
+import Navbar from '@/components/layout/Navbar';
 
 
 export const metadata: Metadata = {
