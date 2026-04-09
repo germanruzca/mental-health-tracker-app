b54e2f7c3da321f8d99d3e12271240e300d957c1
German Ruiz <germanruzca@gmail.com>
2026-04-08 18:09:16 -0600

    client: add configs file

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compiler: {
+    styledComponents: true,
+  },
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
